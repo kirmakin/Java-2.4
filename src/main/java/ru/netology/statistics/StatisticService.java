99950a3647c7f7fddab605449ef2ab8f9eb10c9a
Kirill <kiruxastar@gmail.com>
2020-09-15 16:18:46 +0300

    Initial commit

---
+++ src/main/java/ru/netology/statistics/StatisticService.java
@@ -0,0 +1,67 @@
+package ru.netology.statistics;
+
+public class StatisticService {
+    public int calculateSum(int[] sales) {
+        int sum = 0;
+        for (int sale : sales) {
+            sum += sale;
+        }
+        return sum;
+    }
+
+    public int calculateAverage(int[] sales) {
+        int sum = calculateSum(sales);
+        int average = sum / sales.length;
+        return average;
+    }
+
+
+    public int findMax(int[] sales) {
+        int numberMaxMonth = 0;
+        int maxSale = sales[0];
+        for (int i = 0; i < sales.length; i++) {
+            if (maxSale <= sales[i]) {
+                maxSale = sales[i];
+                numberMaxMonth = i + 1;
+            }
+        }
+        return numberMaxMonth;
+    }
+
+    public int findMin(int[] sales) {
+        int numberMinMonth = 0;
+        int minSale = sales[0];
+        for (int i = 0; i < sales.length; i++) {
+            if (minSale >= sales[i]) {
+                minSale = sales[i];
+                numberMinMonth = i + 1;
+            }
+        }
+        return numberMinMonth;
+    }
+
+    public int moreAverage(int[] sales) {
+        int average = calculateAverage(sales);
+        int summore = 0;
+        for (int sale : sales) {
+            if (sale > average) {
+                sale = 1;
+                summore += sale;
+            }
+        }
+        return summore;
+    }
+
+
+    public int lessAverage(int[] sales) {
+        int average = calculateAverage(sales);
+        int sumless = 0;
+        for (int sale : sales) {
+            if (sale < average) {
+                sale = 1;
+                sumless += sale;
+            }
+        }
+        return sumless;
+    }
+}
---
+++ src/test/java/ru/netology/statistics/StatisticServiseTest.java
@@ -0,0 +1,62 @@
+package ru.netology.statistics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatServiceTest {
+    @Test
+    void shouldCalculateSum() {
+        StatisticService service = new StatisticService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 180;
+        int actual = service.calculateSum(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateAverage() {
+        StatisticService service = new StatisticService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+        int actual = service.calculateAverage(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateMax() {
+        StatisticService service = new StatisticService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
+        int actual = service.findMax(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateMin() {
+        StatisticService service = new StatisticService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 9;
+        int actual = service.findMin(sales);
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void shouldCalculateMAverage() {
+        StatisticService service = new StatisticService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.moreAverage(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateLAverage() {
+        StatisticService service = new StatisticService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+        int actual = service.lessAverage(sales);
+        assertEquals(expected, actual);
+    }
+}
